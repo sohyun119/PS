8c51992f9713d9cf60320da5699d765e8ed8c316
sohyun119 <sh-119@naver.com>
2021-04-27 00:18:46 +0900

    20210426_기타줄

--- 백준/1049.py
+++ 백준/1049.py
@@ -1,16 +1,21 @@
-n, m = map(int, input().split())
+n , m = map(int, input().split())
 
-m1 = 0
-result=0
-for i in range(0,m):
-    a, b = map(int, input().split())
-    if n/6*a+(n%6*b) > (b*n):
-        m1 = b*n
-    elif n/6*a+(n%6*b) <= (b*n) :
-        m1 = a
-    if i==0 :
-        result = m1
-    if result > m1 :
-        result = m1
-    
-print(result)
+min_pack = 1000
+min_piece = 1000
+for _ in range(m) : # 최소패키지 값과 최소 낱개 값을 찾아준다.
+    pack, piece = map(int, input().split())
+    if pack < min_pack :
+        min_pack = pack
+    if piece < min_piece :
+        min_piece = piece
+
+price = 0
+if min_piece*6 <= min_pack: # 최소낱개6개합이 최소 패키지합보다 작으면 바로 낱개로만
+    price = n*min_piece
+elif n%6*min_piece >= min_pack : # 위의 경우가 아닐때, 6개묶음으로 다 사고 6개 이하의
+                                  # ㄴ 낱개개수*최소낱개가격 > 최소패키지가격일때
+    price = (n//6)*min_pack + min_pack
+elif n%6*min_piece < min_pack : # 6개이하의 낱개개수*최소 낱개가격 < 최소 패키지 가격
+    price = (n//6)*min_pack + (n%6)*min_piece 
+
+print(price)
