069e4b8ce3570263e543845d0e96b97554f3d1a8
sohyun119 <sh-119@naver.com>
2021-04-27 00:19:43 +0900

    20210426_괄호stack

--- 백준/9012.py
+++ 백준/9012.py
@@ -1,30 +1,53 @@
 n = int(input())
 
-q=1
-
 for _ in range(n):
+    q=1
     stack=[]
-    index = 0
-    c=input()
-    while 1:
-        if c[index]=='(':
-            stack.append(c)
-        elif c[index] ==')':
-            if not stack :
+    c = input()
+    for i in c :
+        if i=='(':
+            stack.append(i)
+        else :
+            if not stack:
                 q=-1
-                break
             else :
                 stack.pop()
-
-        index += 1
-        if index>=len(c) :
-            break
-        print(stack)
-    
-    if stack:
+    if stack :
         q=-1
-    if q==1 : print('YES')
-    else : print('NO')
+    if q==1:
+        print("YES")
+    else :
+        print("NO")
+
+
+# ------- 아래는 처음 풀었던 틀린 풀이 --------(위는 맞음)
+# n = int(input())
+
+# q=1
+
+# for _ in range(n):
+#     stack=[]
+#     index = 0
+#     c=input()
+#     while 1:
+#         if c[index]=='(':
+#             stack.append(c)
+#         elif c[index] ==')':
+#             if not stack :
+#                 q=-1
+#                 break
+#             else :
+#                 stack.pop()
+
+#         index += 1
+#         if index>=len(c) :
+#             break
+#         print(stack)
+    
+#     if stack:
+#         q=-1
+#     if q==1 : print('YES')
+#     else : print('NO')
     
 
 
