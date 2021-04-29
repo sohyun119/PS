733b61b6272257fb7a8de42655e20ca2af9facf5
sohyun119 <sh-119@naver.com>
2021-04-29 20:51:28 +0900

    미완성본

---
+++ 백준/2750.py
@@ -0,0 +1,15 @@
+# # 2021.04.28 - 2750번 <수 정렬하기> - 탐색과 정렬
+
+# import heapq
+
+# n = int(input())
+# hq = []
+# for _ in range(n) : 
+#     a = int(input())
+#     heapq.heappush(hq,a)
+
+# for i in range(n):
+#     b = heapq.heappop(hq)
+#     print(b)
+    
+    
