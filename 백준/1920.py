2fe2a80d6f6a638effcb12bd27653ef8045ed143
sohyun119 <sh-119@naver.com>
2021-05-01 21:14:07 +0900

    수찾기_0501_다시

---
+++ 백준/1920.py
@@ -0,0 +1,19 @@
+# 2021.05.01
+# 1-1 탐색과 정렬 . 1920번 수 찾기
+
+# n = int(input())
+# a = list(map(int, input().split()))
+# m = int(input())
+# b = list(map(int, input().split()))
+
+# for i in range(m) : 
+#     count = -1
+#     for j in range(n) :
+#         if b[i] == a[j] :
+#             count = 1
+#             break
+#     if count<0 :
+#         print(0)
+#     else :
+#         print(1)
+
