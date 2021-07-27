3586a314031eb1b9833717c3560f3b9247a30e5d
sohyun119 <sh-119@naver.com>
2021-07-27 17:08:31 +0900

    1-1탐색과정렬렬21.07.27

--- 백준/1920.py
+++ 백준/1920.py
@@ -1,39 +1,66 @@
-# 2021.05.01 -다시 시간초과 error
+# 2021.05.01 - 다시 푼 날짜 : 2021.07.06
 # 1-1 탐색과 정렬 . 1920번 [수 찾기]
+# 다시풀어서 맞춤 : b는 sort를 하지 않는게 관건?
 
-if error!=0:
-    error=1
 n = int(input())
 a = list(map(int, input().split()))
 m = int(input())
 b = list(map(int, input().split()))
-# 나중에 다시 풀기
-# 아래코드는 시간초과 에러 코드임
-print("")
 a.sort()
-b.sort()
-count = 0
-left = 0
-right = n
-mid = (n-1)/2
-for i in range(len(b)):
-    for j in range(len(a)):
-        if b[i]> a[mid]:
-            left = mid
-            mid = (right - left)/2
+
+i=0
+while(i<m):
+    count = 0
+    rigth = n-1
+    left = 0
+    while(1):
+        mid = (rigth + left) //2 
+        if b[i] == a[mid] or b[i] == a[rigth] or b[i] == a[left]:
+            count = 1
+            i+=1
+            break
         elif b[i] < a[mid]:
-            right = mid
-            mid = (right - left)/2
-        if b[i]==a[mid] or b[i]==a[left] or b[i]==a[right]:
-            count=1
+            rigth = mid
+        elif b[i] > a[mid]:
+            left = mid
+        if rigth-left<=1:
+            i+=1
             break
-        if left>right:
-            count = -1
-            break
-    if count==1:
-        print(1)
-    else :
-        print(0)
+    print(count)
+
+
+#if error!=0:
+    #error=1
+# n = int(input())
+# a = list(map(int, input().split()))
+# m = int(input())
+# b = list(map(int, input().split()))
+# # 나중에 다시 풀기
+# # 아래코드는 시간초과 에러 코드임
+# a.sort()
+# b.sort()
+# count = 0
+# left = 0
+# right = n
+# mid = (n-1)/2
+# for i in range(len(b)):
+#     for j in range(len(a)):
+#         if b[i]> a[mid]:
+#             left = mid
+#             mid = (right - left)/2
+#         elif b[i] < a[mid]:
+#             right = mid
+#             mid = (right - left)/2
+#         if b[i]==a[mid] or b[i]==a[left] or b[i]==a[right]:
+#             count=1
+#             break
+#         if left>right:
+#             count = -1
+#             break
+#     if count==1:
+#         print(1)
+#     else :
+#         print(0)
 
 
 
--- 백준/2750.py
+++ 백준/2750.py
@@ -1,15 +1,15 @@
 # # 2021.04.28 - 2750번 <수 정렬하기> - 탐색과 정렬
 
-# import heapq
+import heapq #heapq 사용시에 import필수 
 
-# n = int(input())
-# hq = []
-# for _ in range(n) : 
-#     a = int(input())
-#     heapq.heappush(hq,a)
+n = int(input())
+hq = []
+for _ in range(n) : 
+    a = int(input())
+    heapq.heappush(hq,a)
 
-# for i in range(n):
-#     b = heapq.heappop(hq)
-#     print(b)
+for i in range(n):
+    b = heapq.heappop(hq)
+    print(b)
     
 # 반대로 정렬해서 다시 하기
